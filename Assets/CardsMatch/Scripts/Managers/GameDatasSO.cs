4514ee5da1f7b7ee752d6121829e45a40e3b3555
chintu <65482265+chintu0259@users.noreply.github.com>
2024-04-19 00:56:53 +0530

    Created GameData Scriptable Object and added card sprites

---
+++ Assets/CardsMatch/Scripts/Managers/GameDatasSO.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameData", menuName = "Caliberly/GameData")]
+public class GameDatasSO : ScriptableObject
+{
+
+    [SerializeField]public List<CardSO> level;
+    [SerializeField] public List<CardSO> cards;
+
+    [System.Serializable]
+    public class Level
+    {
+        [SerializeField] public int rows;
+        [SerializeField] public int columns;
+        [SerializeField] public int preferredPaddingTopBottom;
+        [SerializeField] public Vector2 spacing;
+    }
+
+    [System.Serializable]
+    public class CardSO
+    {
+        [SerializeField] public string cardName;
+        [SerializeField] public string pairName;
+        [SerializeField] public Sprite cardImage;
+
+        public bool IsPair(string givenName)
+        {
+            givenName = givenName.ToLower();
+
+            return (givenName == pairName);
+        }
+    }
+}
