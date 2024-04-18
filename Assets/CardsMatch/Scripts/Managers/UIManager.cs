8f9a928aed27fc1dad6bce8689cae177bc06f67f
chintu <65482265+chintu0259@users.noreply.github.com>
2024-04-19 00:06:25 +0530

    Added canvas with basic panels

--- Assets/CardsMatch/Scripts/Managers/UIManager.cs
+++ Assets/CardsMatch/Scripts/Managers/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -7,7 +8,8 @@ public class UIManager : MonoBehaviour
     public GameObject Panel_HUD;
     public GameObject Panel_GameOver;
 
-
+    public TextMeshProUGUI text_matches;
+    public TextMeshProUGUI text_moves;
     void TurnOFFAllPanels()
     {
         Panel_HUD.SetActive(false);
@@ -26,4 +28,14 @@ public class UIManager : MonoBehaviour
         TurnOFFAllPanels();
         Panel_GameOver.SetActive(true);
     }
+
+    public void UpdateMatchesText(int matchesCount)
+    {
+        text_matches.text = matchesCount.ToString();
+    }
+    public void UpdateMovesText(int movesCount)
+    {
+        text_moves.text = movesCount.ToString();
+    }
+
 }
